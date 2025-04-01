b2a08ac03a2153086e378271af42a33b219afe7d
Zekihan AZMAN <zekihan_azman@hotmail.com>
2025-04-01 05:27:29 +0300

    fetch Cloudflare IPs dynamically and update initialization logic

--- helpers/cf-ips.go
+++ helpers/cf-ips.go
@@ -2,44 +2,71 @@ package helpers
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/http"
+	"strings"
 )
 
-var CF_IPS = make([]*net.IPNet, len(CF_IPS_STR), len(CF_IPS_STR))
-var CF_IPS_STR = []string{
-	"173.245.48.0/20",
-	"103.21.244.0/22",
-	"103.22.200.0/22",
-	"103.31.4.0/22",
-	"141.101.64.0/18",
-	"108.162.192.0/18",
-	"190.93.240.0/20",
-	"188.114.96.0/20",
-	"197.234.240.0/22",
-	"198.41.128.0/17",
-	"162.158.0.0/15",
-	"104.16.0.0/13",
-	"104.24.0.0/14",
-	"172.64.0.0/13",
-	"131.0.72.0/22",
-	"2400:cb00::/32",
-	"2606:4700::/32",
-	"2803:f800::/32",
-	"2405:b500::/32",
-	"2405:8100::/32",
-	"2a06:98c0::/29",
-	"2c0f:f248::/32",
-}
+var CF_IPS = make([]*net.IPNet, 0)
 
 func init() {
-	for i, cidr := range CF_IPS_STR {
-		_, block, err := net.ParseCIDR(cidr)
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Error parsing CIDR %s: %v", cidr, err))
-		}
-		CF_IPS[i] = block
+	err := getCFIP4()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Error fetching Cloudflare IPs: %v", err))
 	}
+	err = getCFIP6()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Error fetching Cloudflare IPs: %v", err))
+	}
+}
+
+func getCFIP4() error {
+	err := getCFIP("https://www.cloudflare.com/ips-v4")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getCFIP6() error {
+	err := getCFIP("https://www.cloudflare.com/ips-v6")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getCFIP(url string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		log.Fatal(fmt.Sprintf("Error fetching Cloudflare IPs: %s", response.Status))
+	}
+
+	bytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	body := string(bytes)
+
+	lines := strings.Split(body, "\n")
+
+	for _, line := range lines {
+		cidr := strings.TrimSpace(line)
+		if cidr != "" {
+			_, block, err := net.ParseCIDR(cidr)
+			if err != nil {
+				log.Fatal(fmt.Sprintf("Error parsing CIDR %s: %v", cidr, err))
+			}
+			CF_IPS = append(CF_IPS, block)
+		}
+	}
+	return nil
 }
 
 func IsCFIP(ip net.IP) bool {
